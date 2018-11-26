e40f50e0c14491d003ec30637b438cb501df79aa
Philipp Vogt <philippvogt@gmx.at>
2018-11-26 14:30:00 +0100

    fix and add Docstrings

--- work/Aufgabe1_basic/init_surface.py
+++ work/Aufgabe1_basic/init_surface.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Init function for surface
+
+Module functions:
+    init_surface(x): returns y value with cosine function
 """
 
 
@@ -8,7 +11,10 @@ import numpy as np
 
 
 def init_surface(x):
-    '''calculate start coordinates after formula. Crop values at +/- 25.'''
+    """calculate start coordinates after formula. Crop values at +/- 25.
+    
+    :param x: x values of surface
+    """
     mask = np.where(np.abs(x) < 25)
     y = np.zeros(x.size)
     y[mask] = -50 * (1 + np.cos((2 * np.pi * x[mask]) / 50))
--- work/Aufgabe1_basic/surface.py
+++ work/Aufgabe1_basic/surface.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Surface object and functions for miniTopSim
+
+Class functions:
+    normal(): calculates the normal vector and returns it
+    plot(tend, dtime): plots the start and given values
+    write(file, time): writes output file
 """
 
 
@@ -13,28 +18,35 @@ from init_surface import init_surface
 class Surface:
     
     def __init__(self):
-        '''Init the object. Copy start value for printing.'''
+        """Init the object. Copy start value for printing."""
         self.x = np.linspace(-50, 50, 101)
         self.y = np.array(init_surface(self.x))
         self.startx = np.copy(self.x)
         self.starty = np.copy(self.y)
         
     def normal(self):
-        '''Calc normal vector and return it.'''        
+        """Calc normal vector and return it."""        
         x = np.concatenate(([self.x[0]], self.x, [self.x[-1]]))
         y = np.concatenate(([self.y[0]], self.y, [self.y[-1]]))
-        dx = self.calc_vector(x)
-        dy = self.calc_vector(y)
+        dx = self._calc_vector(x)
+        dy = self._calc_vector(y)
         length = np.linalg.norm([dx,dy],axis=0)
         return dy/length, -dx/length
     
-    def calc_vector(self, value):
-        '''Subtract end coordinate with start coordinate.'''
+    def _calc_vector(self, value):
+        """Subtract end coordinate with start coordinate.
+        
+        :param value: coordinate array
+        """
         delta = value[2:] - value[:-2]
         return delta
         
     def plot(self, tend, dtime):
-        '''Plot figure with start and end values.'''
+        """Plot figure with start and end values.
+        
+        :param tend: end time
+        :param dtime: current timestep
+        """
         fig = plt.figure()
         plt.title('miniTopSim')
         plt.grid()
@@ -45,7 +57,11 @@ class Surface:
         fig.savefig('basic_{}_{}.png'.format(int(tend), int(dtime)), dpi=500)
         
     def write(self, file, time):  
-        '''Write output file.'''
+        """Write output file.
+        
+        :param file: filename
+        :param time: current time
+        """
         with open(file, 'a') as fp:
             fp.write('surface: {} {} x-positions y-positions\n'.format(time, len(self.x)))
             for x, y in zip(self.x, self.y):
