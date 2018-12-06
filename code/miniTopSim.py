2f3d200129e6f76a8043de44cce5d983c4ed3353
extremeCrazyCoder <MKich@gmx.at>
2018-12-06 23:28:04 +0100

    done point 2 and 3 added the function distance inside surface.py to compare surfaces added the function load inside surface.py to load saved surfaces fixed docstrings at plot.py (wrong since 59bf5ae) fixed a warning that floats are used without cast inside surface.py (wrong since 5893778) added two tests to show that the distance function is working
    minitopsim file can now be used as module (moved everything into a function)

--- code/miniTopSim.py
+++ code/miniTopSim.py
@@ -12,35 +12,45 @@ import advance  as adv
 import parameters as par
 import plot
 
-
-try:
-    par.set_parameters(sys.argv[1])
-except FileNotFoundError as err:
-    print(err)
-    sys.exit(-1)
-except IndexError:
-    print('No file specified as systemargument')
-    sys.exit(-1)
-
-config = "config"
-if(sys.argv[1].find(".cfg") != -1):
-    config = sys.argv[1].replace(".cfg","")
+def simulate(config_file):
+    """
+    Performs a simulation with the parameters given inside the config file
     
-tend = float(par.TOTAL_TIME)
-dt = float(par.TIME_STEP)
-
-time = 0
-    
-surface = Surface()   
-
-surface.write('{}_{}_{}.srf'.format(config, int(tend), int(dt)), time, 'w')    
-     
-while time < tend:
+        :param config_file: parameters for simulation
         
-    adv.advance(surface,dt)
-    dt = adv.timestep(dt, time, tend)
-    time += dt
-    surface.write('{}_{}_{}.srf'.format(config, int(tend), int(dt)), time, 'a')
+        :return the surface object after last simulation step
+    """
+    try:
+        par.set_parameters(config_file)
+    except FileNotFoundError as err:
+        print(err)
+        sys.exit(-1)
+    
+    config = "config"
+    if(config_file.find(".cfg") != -1):
+        config = config_file.replace(".cfg","")
+        
+    tend = float(par.TOTAL_TIME)
+    dt = float(par.TIME_STEP)
+    time = 0
+    
+    surface = Surface()   
+    surface.write('{}_{}_{}.srf'.format(config, int(tend), int(dt)), time, 'w')    
+         
+    while time < tend:
+        adv.advance(surface,dt)
+        dt = adv.timestep(dt, time, tend)
+        time += dt
+        surface.write('{}_{}_{}.srf'.format(config, int(tend), int(dt)), time, 'a')
+    
+    if par.PLOT_SURFACE:
+        plot.plot('{}_{}_{}.srf'.format(config, int(tend), int(dt)))
+    
+    return surface
 
-if par.PLOT_SURFACE:
-    plot.plot('{}_{}_{}.srf'.format(config, int(tend), int(dt)))
+if __name__ == "__main__":
+    try:
+        simulate(sys.argv[1])
+    except IndexError:
+        print('No file specified as systemargument')
+        sys.exit(-1)
