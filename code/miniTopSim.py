744f35816e005a68e885f85c6b3b13e25d4c770e
Filip Humaj <futureflash94@hotmail.com>
2018-12-10 23:39:56 +0100

    Init commit for Aufgabe 4
    Sputtering added to miniTopSim.
    Table for sputter yield is in tables/syield.dat

--- code/miniTopSim.py
+++ code/miniTopSim.py
@@ -3,15 +3,13 @@
 Main file for miniTopSim
 """
 
-
 import sys
 
-from surface import Surface
-import advance  as adv
-
+import advance as adv
 import parameters as par
 import plot
-
+from sputtering import init_sputtering
+from surface import Surface
 
 try:
     par.set_parameters(sys.argv[1])
@@ -23,24 +21,26 @@ except IndexError:
     sys.exit(-1)
 
 config = "config"
-if(sys.argv[1].find(".cfg") != -1):
-    config = sys.argv[1].replace(".cfg","")
-    
+if sys.argv[1].find(".cfg") != -1:
+    config = sys.argv[1].replace(".cfg", "")
+
 tend = float(par.TOTAL_TIME)
 dt = float(par.TIME_STEP)
 
 time = 0
-    
-surface = Surface()   
 
-surface.write('{}_{}_{}.srf'.format(config, int(tend), int(dt)), time, 'w')    
-     
+init_sputtering()
+surface = Surface()
+
+surface.write('{}_{}_{}.srf'.format(config, int(tend), int(dt)), time, 'w')
+
 while time < tend:
-        
-    adv.advance(surface,dt)
+
+    adv.advance(surface, dt)
     dt = adv.timestep(dt, time, tend)
     time += dt
     surface.write('{}_{}_{}.srf'.format(config, int(tend), int(dt)), time, 'a')
 
 if par.PLOT_SURFACE:
     plot.plot('{}_{}_{}.srf'.format(config, int(tend), int(dt)))
+
