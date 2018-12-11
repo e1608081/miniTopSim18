2dccb1bdfce96a209befe0c2c9e70959feaef7d5
Filip Humaj <futureflash94@hotmail.com>
2018-12-11 22:21:02 +0100

    Add pytest for Aufgabe 4
    Postprocessing of Aufgabe 4:
    Write tests for etch, table and yamamura  with the use of delooping

---
+++ work/Aufgabe4_sputter/test_table.py
@@ -0,0 +1,22 @@
+"""
+Test script that compares newly simulated surface with it's backup
+"""
+
+
+def test_table_10_1():
+    import os
+    import sys
+
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    main_dir = os.path.join(dirname, os.path.pardir, os.path.pardir)
+    code_dir = os.path.join(main_dir, "code")
+    sys.path.insert(0, code_dir)
+    import miniTopSim
+    import surface
+    import plot
+
+    simulated = miniTopSim.simulate(os.path.join(dirname, 'table.cfg'),
+                                    False)
+    saved = surface.load(os.path.join(dirname, 'table_10_1.srf_save'))
+
+    assert simulated.distance(saved) < 0.00215
---
+++ work/Aufgabe4_sputter/test_yamamura.py
@@ -0,0 +1,22 @@
+"""
+Test script that compares newly simulated surface with it's backup
+"""
+
+
+def test_yamamura_10_1():
+    import os
+    import sys
+
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    main_dir = os.path.join(dirname, os.path.pardir, os.path.pardir)
+    code_dir = os.path.join(main_dir, "code")
+    sys.path.insert(0, code_dir)
+    import miniTopSim
+    import surface
+    import plot
+
+    simulated = miniTopSim.simulate(os.path.join(dirname, 'yamamura.cfg'),
+                                    False)
+    saved = surface.load(os.path.join(dirname, 'yamamura_10_1.srf_save'))
+
+    assert simulated.distance(saved) < 0.00215
